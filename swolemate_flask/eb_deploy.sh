72165aa03456264f21e01ed3a3d286024c1d4c99
Kris <johnson.krj@gmail.com>
2021-06-07 19:43:28 -0400

    Renamed deploy script, edited it to deploy an app with naming conventions defined in Application Update 1 pdf

--- swolemate_flask/eb_deploy.sh
+++
@@ -1,2 +0,0 @@
-eb init swolemate-eb-deploy --region us-west-2 --platform Docker --key $PEM_NAME
-eb create swolemate-eb-deploy-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
