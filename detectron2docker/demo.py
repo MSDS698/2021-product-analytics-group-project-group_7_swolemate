48b0e8f8be86667d78837b78526262a3ffbb53e2
Suren Gunturu <surengunturu@MacBook-Pro.local>
2021-06-07 22:18:11 -0700

    merge

--- detectron2docker/demo.py
+++ detectron2docker/demo.py
@@ -11,11 +11,11 @@ import cv2
 import tqdm
 import json
 
-from detectron2.config import get_cfg
+"""from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
 
-from predictor import VisualizationDemo
+from predictor import VisualizationDemo"""
 
 # constants
 WINDOW_NAME = "COCO detections"
