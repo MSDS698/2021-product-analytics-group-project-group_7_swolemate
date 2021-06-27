5046dc29019588b535e84266a8f0a8cfeadfe7bf
kexinwang4483 <47501811+kexinwang4483@users.noreply.github.com>
2021-06-26 23:55:51 -0700

    change docker hub

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-eb init testwkx1 --region us-west-2 --platform Docker --key $PEM_NAME 
-eb create testenvwkx1 --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -i t3.2xlarge
+eb init testv1 --region us-west-2 --platform Docker --key $PEM_NAME 
+eb create testenvv1 --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -i t3.2xlarge
