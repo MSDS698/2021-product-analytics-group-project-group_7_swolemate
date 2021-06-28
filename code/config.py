e52fb94039cfff61053e8560e063f95df3565f60
surengunturumasters <sgunturu@dons.usfca.edu>
2021-06-28 09:30:08 -0700

    change db

--- code/config.py
+++ code/config.py
@@ -1,11 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = """postgresql://postgres:12345678@msds603.
-                                 c08ct9wmukfr.us-west-2.rds.
-                                 amazonaws.com:5432/postgres"""
+    SQLALCHEMY_DATABASE_URI = """postgresql://v2_db:swolemate@msds603v2.c08ct9wmukfr.us-west-2.rds.amazonaws.com/postgres"""
     # flask-login uses sessions which require a secret Key
     SQLALCHEMY_TRACK_MODIFICATIONS = True
