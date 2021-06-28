22a4960cadcb50e89daee1a355b7a3139cfa0060
Moh Kaddoura <mmkaddoura@dons.usfca.edu>
2021-06-27 23:26:37 -0700

    style

--- code/config.py
+++ code/config.py
@@ -1,7 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
-    SECRET_KEY=os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345678@msds603.c08ct9wmukfr.us-west-2.rds.amazonaws.com:5432/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True # flask-login uses sessions which require a secret Key
+    SECRET_KEY = os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = """postgresql://postgres:12345678@msds603.
+                                 c08ct9wmukfr.us-west-2.rds.
+                                 amazonaws.com:5432/postgres"""
+    # flask-login uses sessions which require a secret Key
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
