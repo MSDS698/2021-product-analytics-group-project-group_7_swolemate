abebb4033012776355d7c6160e6e5af751391b9e
surengunturumasters <sgunturu@dons.usfca.edu>
2021-06-28 13:20:58 -0700

    docer

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-eb init group-7-swolemate-app --region us-west-2 --platform Docker --key $PEM_NAME 
-eb create group-7-swolemate-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -i t3.2xlarge
+eb init testapp --region us-west-2 --platform Docker --key $PEM_NAME 
+eb create group-7-swolemate-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -i t3.2xlarge --timeout 30
