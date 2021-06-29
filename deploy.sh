8d13bffee244e9310b6b7c6c5f90561fc6466c99
kexinwang4483 <47501811+kexinwang4483@users.noreply.github.com>
2021-06-28 21:23:01 -0700

    300

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 eb init wkxa1 --region us-west-2 --platform Docker --key $PEM_NAME 
-eb create wkxe2 --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -i t3.2xlarge --timeout 300
+eb create wkxe3 --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -i t3.2xlarge --timeout 300
