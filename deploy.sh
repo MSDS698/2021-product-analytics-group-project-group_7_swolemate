08ab848cffc6e76379f07d4311762fd40910be0a
Suren Gunturu <69869358+surengunturumasters@users.noreply.github.com>
2021-07-21 07:24:10 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-eb init testapp --region us-west-2 --platform Docker --key $PEM_NAME 
-eb create group-7-swolemate-final-site --verbose -i t3.2xlarge
+eb init group-7-swolemate-env --region us-west-2 --platform Docker --key $PEM_NAME 
+eb create group-7-swolemate-env --verbose -i t3.2xlarge
